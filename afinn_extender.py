6550d9cf5634d20317bacd7c1e6d3469b6ec5b22
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2015-04-06 12:16:27 +0200

    added the capability of loading multiple (possibly compressed) corpus files

--- afinn_extender.py
+++ afinn_extender.py
@@ -17,7 +17,8 @@ def load_afinn_file(path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Extends an AFINN words list with similar words as given by a Word2Vec model.'
+        description='Extends an AFINN words list with similar words as given by a Word2Vec model.',
+        epilog='python afinn_extender.py data/AFINN-111.txt data/AFINN-111_extended.txt --corpus_file data/labeledTrainData.tsv.gz --corpus_file data/unlabeledTrainData.tsv.gz --save_word2vec data/w2v_labeled_unlabeled --compression gzip'
     )
     parser.add_argument(
         'afinn_input',
@@ -34,7 +35,9 @@ if __name__ == '__main__':
     )
     group.add_argument(
         '--corpus_file',
-        action='store'
+        action='append',
+        dest='corpora',
+        help='tab-delimited file (with header line) that contains a \'review\' column'
     )
     group.add_argument(
         '--word2vec_model',
@@ -57,23 +60,33 @@ if __name__ == '__main__':
         action='store',
         help='Save the created Word2Vec model'
     )
+    parser.add_argument(
+        '--compression',
+        action='store',
+        default=None,
+        choices=['gzip', 'bz2', None],
+        help='Compression of corpus file(s)'
+    )
+
     args = parser.parse_args()
 
 
-    if args.corpus_file:
+    if args.corpora:
         words_regex = re.compile('\w+', re.UNICODE)
         sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
         stopwords = set(nltk.corpus.stopwords.words('english')) if args.strip_stopwords else set()
 
-        print "Pre-processing unlabled data..."
+        print "Pre-processing data..."
         sentences = []
-        corpus = pandas.read_csv(args.corpus_file, header=0,  delimiter="\t", quoting=3)
-        for review in corpus['review']:
-            review = BeautifulSoup(review).get_text()
-            for line in sentence_tokenizer.tokenize(review):
-                line = line.lower()
-                line = [w for w in words_regex.findall(line) if w not in stopwords]
-                sentences.append(line)
+        for corpus_file in args.corpora:
+            corpus = pandas.read_csv(corpus_file, header=0,  delimiter="\t",
+                                     quoting=3, compression=args.compression)
+            for review in corpus['review']:
+                review = BeautifulSoup(review).get_text()
+                for line in sentence_tokenizer.tokenize(review):
+                    line = line.lower()
+                    line = [w for w in words_regex.findall(line) if w not in stopwords]
+                    sentences.append(line)
 
         print "Training Word2Vec model..."
         w2v = Word2Vec(
