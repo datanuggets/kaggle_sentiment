bf3c9ce77f740073120e99cbcc94e665b46cecad
Kristian Holsheimer <kristian.holsheimer@gmail.com>
2015-04-07 13:21:13 +0200

    added --profile argument

--- validate.py
+++ validate.py
@@ -3,6 +3,15 @@
 import pandas as pd
 import argparse
 from sklearn.metrics import roc_auc_score
+from math import cos, pi
+
+
+def cos_profile(x, n=1):
+    x = (1-cos(pi*x)) / 2
+    while n > 1:
+        x = (1-cos(pi*x)) / 2
+        n -= 1
+    return x
 
 
 if __name__ == '__main__':
@@ -21,6 +30,23 @@ if __name__ == '__main__':
         action='store',
         help='schema: {id: str, sentiment: {0,1}, review: str}, header: yes, delimiter: tab, compression: gzip')
 
+    parser.add_argument(
+        '-p',
+        '--profile',
+        choices=['linear', 'step', 'cos'],
+        action='store',
+        default='linear',
+        help='what kind profile function to apply to the output p = Proba(y=1|x)')
+
+    parser.add_argument(
+        '-n',
+        '--nest',
+        choices=range(1, 10),
+        type=int,
+        action='store',
+        default=1,
+        help='number of nested applications of cosine profile function')
+
     args = parser.parse_args()
 
     validation = pd.read_csv(args.validation_file, sep='\t', index_col='id', compression='gzip')
@@ -30,6 +56,12 @@ if __name__ == '__main__':
     prediction = prediction.join(validation)
 
     y_true = prediction.sentiment.values
-    y_pred = prediction.sentiment_proba.values
+    if args.profile == 'linear':
+        y_pred = prediction.sentiment_proba.values
+    elif args.profile == 'step':
+        y_pred = prediction.sentiment_proba.map(round).values
+    elif args.profile == 'cos':
+        y_pred = prediction.sentiment_proba.map(lambda p: cos_profile(p, args.nest)).values
 
-    print 'ROC AUC score:', roc_auc_score(y_true, y_pred)
+
+    print 'ROC AUC score: %f' % roc_auc_score(y_true, y_pred)
